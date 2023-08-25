61debcf78cf17935f799c7b6bc7e01cb078ae842
Daniel <a20180805@pucp.edu.pe>
2023-08-24 20:46:50 -0500

    base

---
+++ src/main/java/com/lab1/a20180805lab1gtics/Application.java
@@ -0,0 +1,13 @@
+package com.lab1.a20180805lab1gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab1/a20180805lab1gtics/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab1.a20180805lab1gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
